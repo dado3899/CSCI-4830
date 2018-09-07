5d186dd3168e2d4f1cfc134774b336b6bcffc258
David Doan <dado3899@colorado.edu>
2018-09-06 21:00:11 -0600

    Assignment 1
    Above

---
+++ Assignment 1/VR Ball Shinnanigans/Assets/Shrink.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Shrink : MonoBehaviour {
+    private AudioSource audioSource;
+    public AudioClip Scored;
+    public AudioClip Victory;
+    public int CollisionCount;
+    // Use this for initialization
+    void Start () {
+        audioSource = GetComponent<AudioSource>();
+        audioSource.clip = Scored;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+    }
+
+    void OnCollisionEnter(Collision other)
+    {
+        CollisionCount += 1;
+        transform.localScale *= 0.5f;
+        audioSource.Play();
+        if (CollisionCount == 4)
+        {
+            audioSource.loop = true;
+            audioSource.clip = Victory;
+            audioSource.Play();
+            transform.localScale *= 0f;
+            
+        }
+        
+        
+        
+    }
+
+}
